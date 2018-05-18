068eb70cb7b4d96ac62e4eca1800900bda2d348b
Madan <mksiwakoti@gmail.com>
2018-05-17 22:53:31 -0500

    Created certificate and add SSL in the app

---
+++ src/main/java/com/test/madan/httpstest/HttpsTestApplication.java
@@ -0,0 +1,12 @@
+package com.test.madan.httpstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsTestApplication.class, args);
+	}
+}
